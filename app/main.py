effb883139da13e0476898d6adc00c8db260d631
Mikel <mikelherraez@gmail.com>
2026-02-25 17:48:52 +0100

    Actualizacion main

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="API Venta de Entradas")
 
 # DEPENDENCIA DB
-
 def get_db():
     db = SessionLocal()
     try:
@@ -16,21 +15,20 @@ def get_db():
     finally:
         db.close()
 
-
+# ROOT
 @app.get("/")
 def root():
     return {"mensaje": "API de venta de entradas funcionando correctamente"}
 
-# RECINTOS
 
+# RECINTOS 
 @app.post("/recintos/", response_model=schemas.RecintoResponse)
 def crear_recinto(recinto: schemas.RecintoCreate, db: Session = Depends(get_db)):
-    nuevo = models.Recinto(**recinto.dict())
+    nuevo = models.Recinto(**recinto.model_dump())
     db.add(nuevo)
     db.commit()
     db.refresh(nuevo)
     return nuevo
-
 
 @app.get("/recintos/", response_model=list[schemas.RecintoResponse])
 def listar_recintos(db: Session = Depends(get_db)):
@@ -63,6 +61,7 @@ def eliminar_recinto(id: int, db: Session = Depends(get_db)):
     db.commit()
     return {"mensaje": "Recinto eliminado correctamente"}
 
+
 # EVENTOS
 
 @app.post("/eventos/", response_model=schemas.EventoResponse)
@@ -72,45 +71,33 @@ def crear_evento(evento: schemas.EventoCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="El precio no puede ser negativo")
 
     recinto = db.query(models.Recinto).get(evento.recinto_id)
-
     if not recinto:
         raise HTTPException(status_code=404, detail="Recinto no encontrado")
 
-    nuevo = models.Evento(**evento.dict())
+    nuevo = models.Evento(**evento.model_dump())
     db.add(nuevo)
     db.commit()
     db.refresh(nuevo)
     return nuevo
 
-
 @app.get("/eventos/", response_model=list[schemas.EventoResponse])
 def listar_eventos(ciudad: str = None, db: Session = Depends(get_db)):
-
     query = db.query(models.Evento).join(models.Recinto)
-
     if ciudad:
         query = query.filter(models.Recinto.ciudad.ilike(f"%{ciudad}%"))
-
     return query.all()
-
 
 @app.patch("/eventos/{id}/comprar")
 def comprar_tickets(id: int, compra: schemas.CompraRequest, db: Session = Depends(get_db)):
 
     evento = db.query(models.Evento).get(id)
-
     if not evento:
         raise HTTPException(status_code=404, detail="Evento no encontrado")
 
     recinto = evento.recinto
-
     if evento.tickets_vendidos + compra.cantidad > recinto.capacidad:
-        raise HTTPException(
-            status_code=400,
-            detail="Aforo insuficiente en el recinto"
-        )
+        raise HTTPException(status_code=400,detail="Aforo insuficiente en el recinto")
 
     evento.tickets_vendidos += compra.cantidad
     db.commit()
-
     return {"mensaje": "Compra realizada correctamente"}
