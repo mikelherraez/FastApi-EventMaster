16ecba66d4bf9894924e402a6ac623d3c74d15f7
Mikel <mikelherraez@gmail.com>
2026-02-25 18:23:36 +0100

    Añadir check y print para DATABASE_URL en database.py

--- app/database.py
+++ app/database.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL =", DATABASE_URL)
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL no está definida. "
         "Asegúrate de añadirla en tu archivo .env o en Railway Environment Variables"
