ef2637fac7bc1d600bdd82bc05b4c661fa3adf65
mikelherraez <159911722+mikelherraez@users.noreply.github.com>
2026-02-25 19:26:11 +0100

    Remove print statement for DATABASE_URL
    Remove print statement for DATABASE_URL.

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print("DATABASE_URL =", DATABASE_URL)
+
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL no está definida. "
         "Asegúrate de añadirla en tu archivo .env o en Railway Environment Variables"
